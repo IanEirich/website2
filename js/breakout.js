10326cda5a65acb56573dbdced776ed46300ddad
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-03-22 03:04:39 +0000

    Thu, Mar 21, 2024, 8:04 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -10,9 +10,41 @@ ball = {
     y: canvas.height / 2,
     size: 10,
     speed: 4,
-    dx: 4;
-    dy: -4;
+    dx: 4,
+    dy: -4,
 }
+
+// Create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h:  10,
+    speed: 8,
+    dx: 0,
+
+}
+
+// Draw ball on canvas
+function drawBall() {
+  ctx.beginPath()
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+  ctx.fillStyle = '#009599'
+  ctx.fill()
+  ctz.closePath()
+}
+
+// Draw paddle on canvas
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.h)
+    ctx.fillStyle = '#009599'
+    ctx.fill()
+    ctz.closePath()
+  }
+
+drawPaddle()
+drawBall()
 
 // Rules open and close event handlers
 rulesBtn.addEventListener('click', () => {
