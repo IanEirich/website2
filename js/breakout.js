d2ad87c3ef3f38696483fa2e6fd64a037a9b610f
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-03-22 02:56:55 +0000

    Thu, Mar 21, 2024, 7:56 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,16 @@ closeBtn = document.getElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
 // Rules open and close event handlers
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
