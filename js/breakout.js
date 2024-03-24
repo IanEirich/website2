6555ce8b165d819f6e4d21eea716275647d3172a
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-03-24 03:47:10 +0000

    Sat, Mar 23, 2024, 8:47 PM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -101,6 +101,15 @@ function draw() {
 // Move paddle on canvas
 function movePaddle() {
     paddle.x = paddle.x + paddle.dx
+
+    // wall detection
+    if (paddle.x < 0) {
+        paddle.x = 0
+    }
+    if (paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w
+    }
+
 }
 
 // Keydown Event
