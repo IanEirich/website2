87d3563e1c733ed68b9008d1593418ec76f51a21
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-04-12 04:37:32 +0000

    Thu, Apr 11, 2024, 9:37 PM -07:00

--- js/insect.js
+++ js/insect.js
@@ -31,6 +31,15 @@ function startGame() {
 
 function increaseTime() {
     let m = Math.floor(seconds / 60)
+    let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds++
 }
 
 function createInsect() {
