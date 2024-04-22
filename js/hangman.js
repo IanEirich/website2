2b61e4c112f43ca9377edde5105c5ff7ac53b15f
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-04-22 21:43:40 +0000

    Mon, Apr 22, 2024, 2:43 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -6,7 +6,7 @@ const notification = document.getElementById('notification-container')
 const finalMessage = document.getElem'ntById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application", 'programming', interface', 'wizard']
+const word = ['application", 'programming', 'interface', 'wizard']
 
 let selectedIndex= Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
