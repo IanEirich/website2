bf3897ed8be208005b767dce8dbc6b28c06ec2d5
Ian Eirich <142706347+IanEirich@users.noreply.github.com>
2024-04-23 05:50:05 +0000

    Mon, Apr 22, 2024, 10:50 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -11,8 +11,6 @@ const word = ['application', 'programming', 'interface', 'wizard']
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
 
-console.log(selectedWord)
-
 const correctLetters = []
 const wrongLetters = []
 
